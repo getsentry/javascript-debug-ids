99520974c7e4831ea8068c531a3cfbd9a4c1db33
Tim Fish <tim@timfish.uk>
2024-10-02 13:18:58 +0200

    feat: Add basic polyfills

---
+++ src/browser.ts
@@ -0,0 +1,10 @@
+import { getDebugIdFromString } from './common';
+
+export async function getDebugIdForUrl(url: string): Promise<string | undefined> {
+  try {
+    const text = await fetch(url, { cache: 'force-cache' }).then((res) => res.text());
+    return getDebugIdFromString(text);
+  } catch (_) {}
+
+  return;
+}
